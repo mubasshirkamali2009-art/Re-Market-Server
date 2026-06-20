97372c99e703804bfaa2ce0e1f0d9c2dde40c3f6
mubasshir <mubasshirkamali2009@gmail.com>
2026-06-20 21:18:33 +0600

    setup and productsCollection done

---
+++ index.js
@@ -0,0 +1,85 @@
+const dns = require("node:dns");
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+const express = require('express');
+const cors = require('cors');
+const app = express()
+const port = 5000
+require('dotenv').config()
+
+
+app.use(cors())
+app.use(express.json());
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+const uri = process.env.MONGO_DB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+
+
+
+   const database = client.db("re-market");
+    const  productsCollections= database.collection("products");
+
+
+    
+
+app.post('/products'  , async(req, res) => {
+    const product = req.body;
+    const result = await productsCollections.insertOne(product);
+    res.send(result);
+})
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
